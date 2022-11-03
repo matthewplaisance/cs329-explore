886d419df17b4f1905208c15af12c5c9d3544925
Rbinning <116735871+Rbinning@users.noreply.github.com>
2022-11-03 13:32:44 -0500

    Added image view, title and description to home screen

--- exploreAustin/HomeViewController.swift
+++ exploreAustin/HomeViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 
-
 class HomeViewController: UIViewController {
-
+    
+    var images : [String] = ["ZilkerPark", "MountBonnell" ]
+    var imageTitles : [String] = ["Zilker Park", "Mount Bonnell", ]
+    var imageDescription : [String] = ["Zilker Metropolitan Park is considered \("Austin's most-loved park.") This 351-acre metropolitan park is home to a variety of recreation opportunities, facilities and special events for individuals and families.", "Mount Bonnell is one of the highest points in Austin at 781 feet! The peak is named for George Bonnell, who served as Commissioner of Indian Affairs for the Texas Republic."]
+    var imageNumber = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewWillAppear(_ animated: Bool) {
         if DarkMode.darkModeIsEnabled == true{
             overrideUserInterfaceStyle = .dark
@@ -19,10 +29,23 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        changePicture()
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            self.changePicture()
+        }
 
         // Do any additional setup after loading the view.
     }
     
+    func changePicture(){
+        if imageNumber >= images.count{
+            imageNumber = 0
+        }
+        imageView.image = UIImage(named: images[imageNumber])
+        titleLabel.text = imageTitles[imageNumber]
+        descriptionLabel.text = imageDescription[imageNumber]
+        imageNumber += 1
+    }
 
     /*
     // MARK: - Navigation
